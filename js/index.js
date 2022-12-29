3fd2813e7fb5cb222f91da391b44c6dc9b464f82
sillypears <sillypears@users.noreply.github.com>
2022-12-29 04:31:07 -0500

    Oh again

--- js/index.js
+++ js/index.js
@@ -45,6 +45,7 @@
             }
         })
     })
+    
 })(jQuery);
 
 String.prototype.replaceAt = function (index, replacement) {
@@ -74,3 +75,44 @@ function titleChanger() {
 String.prototype.replaceAt = function (index, replacement) {
     return this.substr(0, index) + replacement + this.substr(index + replacement.length);
 }
+
+function toggle(section) {
+    switch (section) {
+        case "home":
+            $('div#content').html(load_home())
+            break;
+        case "blog":
+            $('div#content').html(load_blog())
+            break;
+        case "hisstory":
+            $('div#content').html(load_hisstory())
+
+            break;
+        default:
+            $('div#content').html("da fuq")
+    }
+};
+
+function load_home() {
+    home = `<div class="text-center">
+        <span class="text-uppercase font-weight-bold">recent news</span>: nothing :)
+    </div>`
+    return home
+};
+
+function load_blog() {
+    blog = `<div class="text-center">
+        stupid fruit likes to make stupid stuff. maybe they won't even be fruits
+    </div>`
+    return blog
+};
+
+function load_hisstory() {
+    hisstory = `
+        <div id="marquee">
+            <p>Honestly just go to Instagram</p>
+        </div>
+    `
+    return hisstory
+}
+
